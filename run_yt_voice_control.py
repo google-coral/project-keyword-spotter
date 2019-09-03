1aa35e77aba7e4c23a11bbf525954c8f6418c91e
Mike Tyka <mtyka@google.com>
2019-09-03 10:11:00 -0700

    Add missing license header.
    (And fix some typos)

--- run_yt_voice_control.py
+++ run_yt_voice_control.py
@@ -1,7 +1,21 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Controls a YouTube using voice commands.
 
 
-Useag:
+Usage:
 Requires YouTube to be running in a browser tab and focus to be on the
 YouTube player.
 
@@ -61,7 +75,7 @@ def main():
   yt_control = YoutubeControl()
   sys.stdout.write("--------------------\n")
   sys.stdout.write("This script will control Youtube.\n")
-  sys.stdout.write("Just ensure that focus is on the youtube player.\n")
+  sys.stdout.write("Just ensure that focus is on the YouTube player.\n")
   sys.stdout.write("--------------------\n")
 
   model.classify_audio(mic, engine,
